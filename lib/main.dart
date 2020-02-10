436f42c44059ac6bbc21557a515b9f6f8a507cf4
John <33640075+jtan89@users.noreply.github.com>
2020-02-09 20:01:14 -0500

    added read write local storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import './StampButton.dart';
+import 'dart:io';
+import 'dart:async';
+import 'package:flutter/foundation.dart';
 
 void main() {
-  runApp(FlowApp());
+  runApp(FlowApp(storage: Storage()));
+}
+
+class Storage {
+  Future<String> get localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get localFile async {
+    final path = await localPath;
+    return File('$path/stamps.txt');
+  }
+
+  Future<int> readData() async {
+    try {
+      final file = await localFile;
+      String contents = await file.readAsString();
+      return int.parse(contents);
+    } catch (e) {
+      return 0;
+    }
+  }
+
+  Future<File> writeData(int counter) async {
+    final file = await localFile;
+    return file.writeAsString('$counter');
+  }
 }
 
 class FlowApp extends StatefulWidget {
+  final Storage storage;
+  FlowApp({Key key, @required this.storage}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
     return _FlowAppState();
   }
 }
 
-//Create an list of objects or text then output it through StampButton.dart as a raised button using index.
-
 
 class _FlowAppState extends State<FlowApp> {
+  int state = 0;
+  Future<Directory> _appDocDir;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.storage.readData().then((int value) {
+      setState(() {
+        state = value;
+      });
+    });
+  }
+
+  //methods to write data to file
+
+  Future<File> writeData() async {
+    setState((){
+      state += 1;
+    });
+    return widget.storage.writeData(state);
+  }
+
+  void getAppDirectory() {
+    setState(() {
+      _appDocDir = getApplicationDocumentsDirectory();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,51 +93,69 @@ class _FlowAppState extends State<FlowApp> {
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),
               ),
             ),
-
             Padding(
               padding: const EdgeInsets.all(18.0),
               child: Text(
                   "The more you float - the better it gets - and the better you feel. Your 10th float is on us!"),
             ),
-         
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children:[
+              children: [
                 Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children:[
-                StampButton(),
-                StampButton(),
-                StampButton(),
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    StampButton(),
+                    StampButton(),
+                    StampButton(),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    StampButton(),
+                    StampButton(),
+                    StampButton(),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    StampButton(),
+                    StampButton(),
+                    StampButton(),
+                  ],
+                ),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children:[
-                StampButton(),
-                StampButton(),
-                StampButton(),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children:[
-                StampButton(),
-                StampButton(),
-                StampButton(),
-              ],
-            ),
-              ],
-            ),
-
             Icon(
               Icons.tag_faces,
               color: Colors.blue,
               size: 50.0,
             ),
+            Text('${state ?? "File is Empty"}'),
+            RaisedButton(onPressed: writeData, child: Text('Write to File')),
+            RaisedButton(onPressed: getAppDirectory, child: Text('Get DIR path')),
+            FutureBuilder<Directory>(
+              future: _appDocDir,
+              builder: (BuildContext context, AsyncSnapshot<Directory> snapshot) {
+                Text text = Text('');
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if(snapshot.hasError) {
+                    text = Text('Error: ${snapshot.error}');
+                  } else if (snapshot.hasData) {
+                    text = Text('path: ${snapshot.data.path}');
+                  } else {
+                    text = Text('unavailable');
+                  }
+                }
+                return new Container (child: text,);
+              },
+            )
           ],
         ),
       ),
     );
   }
 }
+
+
